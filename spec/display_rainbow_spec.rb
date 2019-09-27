9e534e405f6a87b88a98842d356f44e1a5b588c6
generic-puppet-7755 <tasmith1990@gmail.com>
2019-09-27 22:50:14 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -24,8 +24,9 @@ describe '#display_rainbow' do
 end
 
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  
   puts colors
   
-  # "R: red, O: orange, Y: "
+  # "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
   
-  
+end
