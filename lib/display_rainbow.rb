038b10a4a64f24193765d69957b7d092dee5340f
generic-puppet-7755 <tasmith1990@gmail.com>
2019-09-27 22:55:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,15 @@
 
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   
-  puts colors
+  letters = ["R", "O", "Y", "G", "B", "I", "V"]
+  
+  puts colors[0]
+  puts colors[1]
+  puts colors[2]
+  puts colors[3]
+  puts colors[4]
+  puts colors[5]
+  puts colors[6]
   
   # "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
   
